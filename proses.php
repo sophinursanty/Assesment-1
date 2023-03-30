db22de7f4a694a2ff43bfed096c9df93d88bce87
sophie <sophinursanty@studentelkomuniversity.ac.id>
2023-03-30 10:09:17 +0700

    v1.2.1

--- proses.php
+++ proses.php
@@ -1,24 +1,25 @@
 <?php
-// Ambil nilai dari textbox angka1 dan angka2
 $angka1 = $_POST["angka1"];
 $angka2 = $_POST["angka2"];
 
-// Ambil nilai dari dropdown operator
 $operator = $_POST["operator"];
 
-// Lakukan operasi matematika sesuai dengan nilai operator
-if ($operator == "+") {
-	$hasil = $angka1 + $angka2;
-} else if ($operator == "-") {
-	$hasil = $angka1 - $angka2;
-} else if ($operator == "*") {
-	$hasil = $angka1 * $angka2;
-} else if ($operator == "/") {
-	$hasil = $angka1 / $angka2;
-} else {
-	$hasil = "Error: Operator tidak valid";
+switch ($operator) {
+	case "+":
+		$hasil = $angka1 + $angka2;
+		break;
+	case "-":
+		$hasil = $angka1 - $angka2;
+		break;
+	case "*":
+		$hasil = $angka1 * $angka2;
+		break;
+	case "/":
+		$hasil = $angka1 / $angka2;
+		break;
+	default:
+		$hasil = "Error: Operator tidak valid";
 }
 
-// Tampilkan hasil operasi aritmatika
-echo "Hasil:".$hasil;
+echo "Hasil: " . $hasil;
 ?>
